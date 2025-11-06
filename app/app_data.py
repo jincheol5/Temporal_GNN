3ebe0d22c5619370caf716bea3ca1bcf82d30b39
jincheol <oscar4293@gmail.com>
2025-11-06 19:08:01 +0900

    update

--- app/app_data.py
+++ app/app_data.py
@@ -1,16 +1,8 @@
 from tgnn import DataUtils
 
-val_20=DataUtils.DataLoader.load_from_pickle("val_20","graph")
 
-print(val_20.keys())
-
-# val_20=val_20.pop('all',None)
-# DataUtils.DataLoader.save_to_pickle(data=val_20,file_name='val_20',dir_type='graph')
-
-
-# train_20=DataUtils.DataLoader.load_from_pickle("train_20","graph")
-
-# for graph_type,graph_list in train_20.items():
-#     print(f"type: {graph_type}")
-#     print(f"list len: {len(graph_list)}")
-#     print(f"data type: {type(graph_list[0])}")
+train_20=DataUtils.DataLoader.load_from_pickle("train_20","graph")
+print(train_20.keys())
+train_20.pop('all',None)
+print(train_20.keys())
+# DataUtils.DataLoader.save_to_pickle(data=train_20,file_name='train_20',dir_type='graph')
