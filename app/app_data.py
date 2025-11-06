01b6716e786f0d0f10bb7dbb2d25b9f6cb21a24c
jincheol <oscar4293@gmail.com>
2025-11-06 19:16:12 +0900

    update

--- app/app_data.py
+++ app/app_data.py
@@ -6,3 +6,9 @@ print(data.keys())
 data.pop('all',None)
 print(data.keys())
 DataUtils.DataLoader.save_to_pickle(data=data,file_name='test_20',dir_type='graph')
+
+"""
+TO DO LIST:
+1. temporal graph dataset 다시 생성
+2. batch 어떻게 처리해서 효율적으로 구성할지
+"""
