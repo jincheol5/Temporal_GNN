55bb33b13bdcc945845126d95dc5a49065ce3106
jincheol <oscar4293@gmail.com>
2025-11-06 18:55:08 +0900

    update

--- app/app_data.py
+++ app/app_data.py
@@ -6,3 +6,11 @@ for graph_type,graph_list in val_20.items():
     print(f"type: {graph_type}")
     print(f"list len: {len(graph_list)}")
     print(f"data type: {type(graph_list[0])}")
+
+
+train_20=DataUtils.DataLoader.load_from_pickle("train_20","graph")
+
+for graph_type,graph_list in train_20.items():
+    print(f"type: {graph_type}")
+    print(f"list len: {len(graph_list)}")
+    print(f"data type: {type(graph_list[0])}")
