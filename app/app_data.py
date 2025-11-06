c907a68b60c48cd5d70abf26aacf55e380258405
jincheol <oscar4293@gmail.com>
2025-11-06 18:54:06 +0900

    update

--- app/app_data.py
+++ app/app_data.py
@@ -2,4 +2,7 @@ from tgnn import DataUtils
 
 val_20=DataUtils.DataLoader.load_from_pickle("val_20","graph")
 
-print(type(val_20))
+for graph_type,graph_list in val_20.items():
+    print(f"type: {graph_type}")
+    print(f"list len: {len(graph_list)}")
+    print(f"data type: {type(graph_list[0])}")
