c9f9a3f51626ebbdf77355f60252b307af224b86
jincheol5 <oscar4293@gmail.com>
2025-11-15 02:25:10 +0900

    update

--- app/app_analysis.py
+++ app/app_analysis.py
@@ -5,13 +5,35 @@ from tgnn import DataUtils,GraphAnalysis
 
 def app_analysis(config:dict):
     match config['app_num']:
+        case 0:
+            """
+            App 0.
+            """
+            print(f"<<{config['mode']}_{config['num_nodes']}_{config['graph_type']} graphs statistics result>>")
+            
+            data_dict=DataUtils.load_from_pickle(file_name=f"{config['mode']}_{config['num_nodes']}_parallel_{graph_type}",path='trne',dir_type=config['mode'])
+            data_list=[]
+            for _,src_dict in data_dict.items():
+                for _,data in src_dict.items():
+                    data_list.append(data)
+
+            reachability_ratio_list=[]
+            for data in data_list:
+                reachability_ratio_list.append(GraphAnalysis.check_reachability_ratio(r=data.r[-1]))
+            lst=np.array(reachability_ratio_list,dtype=float)
+            mean_ratio=lst.mean()
+            max_ratio=lst.max()
+            min_ratio=lst.min()
+            print(f"{config['mode']}_{config['num_nodes']}_{graph_type} graphs tR ratio mean: {mean_ratio} max: {max_ratio} min: {min_ratio}")
+            print()
+
         case 1:
             """
             App 1.
             Check statistics of num_nodes,num_edge_events,num_static_edges after remove self-loop
             """
             print(f"<<{config['mode']}_{config['num_nodes']} graphs statistics result>>")
-            graph_list_dict=DataUtils.load_from_pickle(file_name=f"{config['mode']}_{config['num_nodes']}",dir_type="graph")
+            graph_list_dict=DataUtils.load_from_pickle(file_name=f"{config['mode']}_{config['num_nodes']}",path='trne',dir_type="graph")
             for graph_type,graph_list in graph_list_dict.items():
                 N_list=[]
                 E_s_list=[]
@@ -36,7 +58,7 @@ def app_analysis(config:dict):
             graph_type_list=['ladder','grid','tree','erdos_renyi','barabasi_albert','community','caveman']
             for graph_type in graph_type_list:
                 data_list=[]
-                data_dict=DataUtils.load_from_pickle(file_name=f"{config['mode']}_{config['num_nodes']}_parallel_{graph_type}",dir_type=config['mode'])
+                data_dict=DataUtils.load_from_pickle(file_name=f"{config['mode']}_{config['num_nodes']}_parallel_{graph_type}",path='trne',dir_type=config['mode'])
                 for _,src_dict in data_dict.items():
                     for _,data in src_dict.items():
                         data_list.append(data)
