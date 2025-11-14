83e416e94c16f6d1746249f5d329592bca8d0237
jincheol5 <oscar4293@gmail.com>
2025-11-15 02:33:52 +0900

    update

--- tgnn/metrics.py
+++ tgnn/metrics.py
@@ -64,6 +64,9 @@ class Metrics:
         """
         pred=torch.sigmoid(logit) # [N,1]
         pred_label=(pred>=0.5).float() # [N,1]
+
+        print(f"pred_label: {pred_label}")
+
         correct=(pred_label==label.float()).float() # [N,1]
         acc=correct.mean() # scalar []
         return acc.cpu().item()
