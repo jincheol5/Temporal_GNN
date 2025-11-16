8b5942c2f459ca02cc5f5430fb29d6df7fb54880
jincheol <oscar4293@gmail.com>
2025-11-16 17:54:39 +0900

    update

--- app/app_data.py
+++ app/app_data.py
@@ -12,38 +12,23 @@ def app_data(config: dict):
         case 1:
             """
             App 1.
-            Convert and save all type graph_list_dict to dataset_dict_list_all_type
-                num_nodes: 20, 50, 100 
+            Convert and save all type graph_list_dict into dataset_list and store them in chunks.
+            Only train
             """
-            # train_20=DataUtils.load_from_pickle(file_name="train_20",path="trne",dir_type="graph")
-            val_20=DataUtils.load_from_pickle(file_name="val_20",path="trne",dir_type="graph")
-            # test_20=DataUtils.load_from_pickle(file_name="test_20",path="trne",dir_type="graph")
-            # test_50=DataUtils.load_from_pickle(file_name="test_50",path="trne",dir_type="graph")
-            # test_100=DataUtils.load_from_pickle(file_name="test_100",path="trne",dir_type="graph")
-
-            # dataset_dict_list_all_type=GraphUtils.convert_to_dataset_dict_list_all_type(graph_list_dict=train_20)
-            # DataUtils.save_dataset_dict_list_all_type(dataset_dict_list_all_type=dataset_dict_list_all_type,file_name="train_20",dir_type="train")
-            # print(f"Finish train_20")
-            
-            dataset_dict_list_all_type=GraphUtils.convert_to_dataset_dict_list_all_type(graph_list_dict=val_20)
-            DataUtils.save_dataset_dict_list_all_type(dataset_dict_list_all_type=dataset_dict_list_all_type,file_name="val_20",dir_type="val")
-            print(f"Finish val_20")
-
-            # dataset_dict_list_all_type=GraphUtils.convert_to_dataset_dict_list_all_type(graph_list_dict=test_20)
-            # DataUtils.save_dataset_dict_list_all_type(dataset_dict_list_all_type=dataset_dict_list_all_type,file_name="test_20",dir_type="test")
-            # print(f"Finish test_20")
-
-            # dataset_dict_list_all_type=GraphUtils.convert_to_dataset_dict_list_all_type(graph_list_dict=test_50)
-            # DataUtils.save_dataset_dict_list_all_type(dataset_dict_list_all_type=dataset_dict_list_all_type,file_name="test_50",dir_type="test")
-            # print(f"Finish test_50")
-
-            # dataset_dict_list_all_type=GraphUtils.convert_to_dataset_dict_list_all_type(graph_list_dict=test_100)
-            # DataUtils.save_dataset_dict_list_all_type(dataset_dict_list_all_type=dataset_dict_list_all_type,file_name="test_100",dir_type="test")
-            # print(f"Finish test_100")
+            graph_list_dict=DataUtils.load_from_pickle(file_name="train_20",path="trne",dir_type="graph")
+            dataset_list_dict=GraphUtils.convert_to_dataset_list_dict(graph_list_dict=graph_list_dict)
+            DataUtils.save_dataset_list_dict(dataset_list_dict=dataset_list_dict,file_name="train_20",chunk_size=config['chunk_size'],dir_type="train")
 
         case 2:
             """
             App 2.
+            Convert and save all type graph_list_dict into each type dataset_list and store them in chunks.
+            Only val, test
+            """
+
+        case 3:
+            """
+            App 3.
             Convert and save single type graph_list to dataset_dict_list 
             """
             graph_list_dict=DataUtils.load_from_pickle(file_name=f"{config['mode']}_{config['num_nodes']}",path="trne",dir_type="graph")
@@ -61,6 +46,7 @@ if __name__=="__main__":
     parser.add_argument("--mode",type=str,default='test')
     parser.add_argument("--num_nodes",type=int,default=20)
     parser.add_argument("--graph_type",type=str,default='default')
+    parser.add_argument("--chunk_size",type=int,default=1)
     args=parser.parse_args()
 
     config={
@@ -68,6 +54,7 @@ if __name__=="__main__":
         'app_num':args.app_num,
         'mode':args.mode,
         'num_nodes':args.num_nodes,
-        'graph_type':args.graph_type
+        'graph_type':args.graph_type,
+        'chunk_size':args.chunk_size
     }
     app_data(config=config)
